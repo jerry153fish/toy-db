664192c0b28007ae268da3c3afae61808fcd2d5f
Jerry Chen <jerry153fish@gmail.com>
2021-06-22 21:31:12 +1000

    add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline("toy_db >> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                match line.as_str() {
+                    "exit" => break,
+                    _ => println!("Line: {}", line),
+                }
+                
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
 }
